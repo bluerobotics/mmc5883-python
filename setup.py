b6c53c028512393a9e6e91cd951352bc213595f4
Jacob Walser <jwalser90@gmail.com>
2021-09-28 08:31:47 -0400

    setup: add entry_points for test and report

--- setup.py
+++ setup.py
@@ -9,6 +9,12 @@ setup(
     author='Blue Robotics',
     url='https://github.com/bluerobotics/mmc5883-python',
     packages=['mmc5883'],
+    entry_points={
+        'console_scripts': [
+            'mmc5883-test=mmc5883.test:main',
+            'mmc5883-report=mmc5883.report:main'
+        ],
+    },
     package_data={ "mmc5883": ["mmc5883.meta"]},
     install_requires=['smbus2'],
 )
